737c5d50c544c1b0182f9306af2a866ac0d19e7d
Arturo Merlano <arturomerlano17@hotmail.com>
2023-09-17 19:41:43 -0400

    Playground de union de conjuntos

---
+++ play_ground_01.py
@@ -0,0 +1,10 @@
+countries = {"MX", "COL", "ARG", "USA"}
+northAm = {"USA", "CANADA"}
+centralAm = {"MX", "GT", "BZ"}
+southAm = {"COL", "BZ", "ARG"}
+
+new_set = set(countries | northAm | centralAm | southAm)
+# Escribe tu solución 👇
+
+
+print(new_set)
