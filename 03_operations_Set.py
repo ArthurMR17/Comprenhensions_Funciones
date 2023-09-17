755085b3384a12c08013cfc40718bb726c1f085d
Arturo Merlano <arturomerlano17@hotmail.com>
2023-09-17 19:33:02 -0400

    Operaciones con Sets, union, interseccion, diferencia y diferencia simetrica

---
+++ 03_operations_Set.py
@@ -0,0 +1,23 @@
+set_a = {"col", "mex", "bol"}
+set_b = {"pe", "bol"}
+
+# metodo Union para unir conjuntos
+set_c = set_a.union(set_b)
+print(set_c)
+print(set_a | set_b)  # operador | para sumar conjuntos o unirlos
+
+# conocer elementos en comun
+set_d = set_a.intersection(set_b)
+print(set_d)
+print(set_a & set_b)  # operador & para intersection
+
+# eliminar el elemento comun
+# del conjunto B en el conjunto A
+set_c = set_a.difference(set_b)
+print(set_c)
+print(set_a - set_b)  # operador para diferencia
+
+# unir los conjuntos sin el elemento en comun
+set_c = set_a.symmetric_difference(set_b)
+print(set_c)
+print(set_a ^ set_b)
